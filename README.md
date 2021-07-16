48ab7fd27a51edb771d84c33e6b12c2ebe412375
pmayil89 <pmayil89@gmail.com>
2021-07-16 21:46:24 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-y2mwav
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-y2mwav)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var o1 = 2;
+var i1="2";
+if(o1===i1)
+{
+  appDiv.innerHTML = true;
+}
+else
+appDiv.innerHTML = false;
